1ca696f9f02187ddb5a5171333b08072ae19e9b5
BhagyaShree43 <bhagyashrisahu19@gmail.com>
2025-12-14 07:32:03 -0500

    linting Python and JavaScript

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -37,8 +37,9 @@ class Migration(migrations.Migration):
                     ('CONVERTIBLE', 'Convertible'),
                     ('PICKUP', 'Pickup')], default='SUV', max_length=20)),
                 ('year', models.IntegerField(default=2023,
-                  validators=[django.core.validators.MaxValueValidator(2025),
-                 django.core.validators.MinValueValidator(2015)])),
+                                            validators=
+                                            [django.core.validators.MaxValueValidator(2025),
+                                            django.core.validators.MinValueValidator(2015)])),
                 ('car_make', models.ForeignKey(
                   on_delete=django.db.models.deletion.CASCADE,
                   to='djangoapp.carmake')),
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -101,8 +101,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation',
-        'django.contrib.auth.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     }    
     {
         'NAME':
